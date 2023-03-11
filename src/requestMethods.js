7defcd217a937106164910d93a6bbf164995f836
godfrey <godfreyndiritu254@gmail.com>
2023-03-11 11:14:52 +0300

    backend route fixing

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -2,7 +2,7 @@ import axios from "axios"
 import { useSelector } from "react-redux"
 import { selectUser } from "./redux/userRedux.js"
 
-const BASE_URL = "https://com-shop.onrender.com/"
+const BASE_URL = "https://com-shop.onrender.com/api/"
 // const token = useSelector(selectUser).token
 
 // const GetToken = () => {
